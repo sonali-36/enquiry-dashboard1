c760a6c37c1124a3c474d6dc772f7e8382d3e03d
sonali-36 <126885382+sonali-36@users.noreply.github.com>
2026-03-18 17:02:02 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+import json
+
 
 # -------------------------------------------------
 # PAGE CONFIG
